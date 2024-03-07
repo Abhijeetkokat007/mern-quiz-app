e5eb271c346aafdb4ae960761e18343d653ed83f
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2024-03-07 08:42:00 +0530

    added acceess

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express()
 // }))
 app.use((req, res, next) => {
   
-  res.setHeader('Access-Control-Allow-Origin' , 'https://mern-quiz-app-self.vercel.app/' ,'http://localhost:5173' );
+  res.setHeader('Access-Control-Allow-Origin' ,'https://mern-quiz-app-self.vercel.app' );
   
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   
